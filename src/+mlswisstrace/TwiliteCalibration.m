b350dd6669b890d0fe6a04f2496d3e7f7741b644
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-09-03 16:44:36 -0500

    added: 	function this = createFromDate(varargin)         function m    = mean_nobaseline(this)         function m    = median_nobaseline(this)         function s     = std_nobaseline(this)         function this = updateTimingData(this, aDatetime)         function this = updateActivities(this) 	ctor body

--- src/+mlswisstrace/TwiliteCalibration.m
+++ src/+mlswisstrace/TwiliteCalibration.m
@@ -1,22 +1,97 @@
-classdef TwiliteCalibration < handle & mlswisstrace.Twilite
+classdef TwiliteCalibration < mlswisstrace.AbstractTwilite
 	%% TWILITECALIBRATION  
 
 	%  $Revision$
  	%  was created 19-Jul-2017 23:38:06 by jjlee,
  	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlswisstrace/src/+mlswisstrace.
  	%% It was developed on Matlab 9.2.0.538062 (R2017a) for MACI64.  Copyright 2017 John Joowon Lee.
- 	
+ 	    
+    methods (Static)
+        function this = createFromDate(varargin)
+            %% @param required dt is datetime.
+            
+            ip = inputParser;
+            addRequired(ip, 'dt', @isdatetime);
+            addParameter(ip, 'isotope', 'FDG', @ischar);
+            parse(ip, varargin{:})
+            ipr = ip.Results;
+
+            crvpth = fullfile(getenv('HOME'), 'Documents', 'private', 'Twilite', 'CRV', '');
+            crvfp = sprintf('fdg_dt%d%02d%02d', ipr.dt.Year, ipr.dt.Month, ipr.dt.Day);
+            crvfqfn = fullfile(crvpth, [crvfp '.crv']);
+            if ~isfile(crvfqfn) 
+                crvfp = sprintf('o15_fdg_dt%d%02d%02d', ipr.dt.Year, ipr.dt.Month, ipr.dt.Day);
+                crvfqfn = fullfile(crvpth, [crvfp '.crv']);
+                assert(isfile(crvfqfn))
+            end
+            crm = mlpet.CCIRRadMeasurements.createByDate(ipr.dt);
+            this = mlswisstrace.TwiliteCalibration( ...
+                'fqfilename', crvfqfn, ...
+                'manualData', crm, ...
+                'isotope', ipr.isotope, ...
+                'doseAdminDatetime', crm.datetimeTracerAdmin('tracer', 'cal'));
+        end
+    end
     
 	methods 
+        function m    = mean_nobaseline(this)
+            m = mean(this.timingData_.activity);
+        end
+        function m    = median_nobaseline(this)
+            m = median(this.timingData_.activity);
+        end
+        function s    = std_nobaseline(this)
+            s = std(this.timingData_.activity);
+        end
+        function this = updateTimingData(this, aDatetime)
+            %% UPDATETIMINGDATA progressively shrinks this.timingData_ by imposing time limits based on
+            %  @param aDatetime.
+            
+            this.timingData_ = this.timingData_.findCalibrationFrom(aDatetime);
+        end
+        function this = updateActivities(this)
+            %% UPDATEACTIVITIES updates counts_ from timingData_ and specificActivity_ with counts2specificActivity_.
+            
+            this.counts_ = this.timingData_.activity;
+            this.specificActivity_ = this.counts2specificActivity_*this.counts_;
+        end 
+        
  		function this = TwiliteCalibration(varargin)
  			%% TWILITECALIBRATION
+            %  @param dt.
+            %  @param invEfficiency.
+            %  @param expectedBaseline.
+            %  @param doMeasureBaseline.
        
- 			this = this@mlswisstrace.Twilite(varargin{:}, 'doMeasureBaseline', false);            
+ 			this = this@mlswisstrace.AbstractTwilite(varargin{:});
+            
             ip = inputParser;
             ip.KeepUnmatched = true;
-            addParameter(ip, 'isotope', '18F', @ischar);
-            parse(ip, varargin{:});                      
-            this.isotope_ = ip.Results.isotope;
+            addParameter(ip, 'dt', 1,                 @isnumeric);
+            addParameter(ip, 'invEfficiency', 1,    @isnumeric);
+            addParameter(ip, 'expectedBaseline', 92,  @isnumeric);
+            addParameter(ip, 'doMeasureBaseline', true, @islogical);
+            parse(ip, varargin{:});
+            ipr = ip.Results;
+            
+            this = this.readtable;
+            ttdt = this.tableTwilite2datetime;
+            this.timingData_ = mlpet.MultiBolusData( ...
+                'activity', this.tableTwilite2coincidence, ...
+                'times', ttdt, ...
+                'datetimeMeasured', ttdt(1), ...
+                'dt', ipr.dt, ...
+                'expectedBaseline', ipr.expectedBaseline, ...
+                'doMeasureBaseline', ipr.doMeasureBaseline, ...
+                'radionuclide', mlpet.Radionuclides(this.isotope));
+            this.counts2specificActivity_ = ipr.invEfficiency;    
+            
+            this = this.updateTimingData(this.doseAdminDatetime); 
+            this = this.updateActivities;
+            this.isDecayCorrected_ = false;
+            this.isPlasma = false;            
+            this = this.updateDecayCorrection;
+            this.isDecayCorrected = true;
  		end
     end 
     
@@ -27,8 +102,7 @@ classdef TwiliteCalibration < handle & mlswisstrace.Twilite
         samplingRange_ % time index
     end
     
-    %% HIDDEN    
-    %  @deprecated
+    %% HIDDEN @deprecated
     
     methods (Hidden)
         function [m,s] = calibrationBaseline(this)
