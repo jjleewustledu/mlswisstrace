d72e24f941eaf7430a2e31cc2312c0344772f597
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2020-03-26 11:05:06 -0500

    TwiliteData is passing all unit tests

--- src/+mlswisstrace/TwiliteData.m
+++ src/+mlswisstrace/TwiliteData.m
@@ -1,4 +1,4 @@
-classdef TwiliteData < handle & AbstractTracerData
+classdef TwiliteData < handle & mlpet.AbstractTracerData
 	%% TWILITEDATA  
 
 	%  $Revision$
@@ -6,20 +6,268 @@ classdef TwiliteData < handle & AbstractTracerData
  	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlswisstrace/src/+mlswisstrace.
  	%% It was developed on Matlab 9.4.0.813654 (R2018a) for MACI64.  Copyright 2018 John Joowon Lee.
  	
-	properties
- 		
- 	end
+    properties (Dependent)
+        baseline % countRate
+        baselineSup
+        pumpRate 
+        tableTwilite % all stored data
+        visibleVolume 
+    end
+    
+    methods (Static)
+        function this = createFromSession(sesd)
+            this = [];
+            assert(isa(sesd, 'mlpipeline.ISessionData'))
+            
+            try
+                this = mlswisstrace.TwiliteData( ...
+                    'isotope', sesd.isotope, ...
+                    'tracer', sesd.tracer, ...
+                    'datetimeMeasured', sesd.datetime);
+                if lstrfind(lower(sesd.tracer), 'fdg')
+                    fn = sprintf('*fdg_dt%s.crv', datestr(sesd.datetime, 'yyyymmdd'));
+                else
+                    fn = sprintf('*o15_dt%s.crv', datestr(sesd.datetime, 'yyyymmdd'));
+                end
+                fqfnCrvs = globT(fullfile(mlnipet.Resources.instance().CCIR_RAD_MEASUREMENTS_DIR, 'Twilite', 'CRV', fn));
+                this.read(fqfnCrvs{1});
+            catch ME
+                handwarning(ME)
+            end
+        end
+    end
 
-	methods 
+	methods
+        
+        %% GET
+        
+        function g = get.baseline(this)
+            g = this.baseline_;
+        end
+        function g = get.baselineSup(~)
+            g = 200;
+        end
+        function g = get.pumpRate(this)
+            %% default := 5 mL/min
+            
+            g = this.pumpRate_;
+        end
+        function g = get.tableTwilite(this)
+            g = this.tableTwilite_;
+        end
+        function g = get.visibleVolume(this)
+            %% of catheter, default := 0.27 mL, default := 0.14 mL for datetime < 20170412
+            
+            if isnan(this.visibleVolume_)
+                this.visibleVolume_ = 0.27; % mL for Braun ref V5424, 38 cm len, 0.642 mL priming vol
+            end
+            if isdatetime(this.datetimeForDecayCorrection) && ...
+                ~isnat(this.datetimeForDecayCorrection) && ...
+                    this.datetimeForDecayCorrection < datetime(2017,4,12, 'TimeZone', mlpipeline.ResourcesRegistry.instance().preferredTimeZone)
+                this.visibleVolume_ = 0.14; % mL for Medex REF 536035, 152.4 cm, clamp APV = 1.1. mL
+            end
+            g = this.visibleVolume_;
+        end
+        
+        %%
+        
+        function a = activity(this, varargin)
+            %% Bq
+            %  @param decayCorrected, default := false.
+ 			%  @param datetimeForDecayCorrection updates internal.
+            
+            a = this.activityOverCountRate_*this.countRate(varargin{:});
+        end
+        function a = activityDensity(this, varargin)
+            %% Bq/mL
+            %  @param decayCorrected, default := false.
+ 			%  @param datetimeForDecayCorrection updates internal.
+            
+            a = this.activity(varargin{:})/this.visibleVolume;
+        end
+        function c = countRate(this, varargin)
+            %% cps
+            %  @param decayCorrected, default := false.
+ 			%  @param datetimeForDecayCorrection updates internal.
+            
+            ip = inputParser;
+            addParameter(ip, 'decayCorrected', false, @islogical)
+            addParameter(ip, 'datetimeForDecayCorrection', NaT, @(x) isnat(x) || isdatetime(x))
+            addParameter(ip, 'index0', this.index0, @isnumeric)
+            addParameter(ip, 'indexF', this.indexF, @isnumeric)
+            parse(ip, varargin{:})
+            ipr = ip.Results;
+            
+            if isdatetime(ipr.datetimeForDecayCorrection) && ...
+                    ~isnat(ipr.datetimeForDecayCorrection)
+                this.datetimeForDecayCorrection = ipr.datetimeForDecayCorrection;
+            end              
+            if ipr.decayCorrected && ~this.decayCorrected
+                this = this.decayCorrect();
+                this.decayCorrected_ = true;
+            end
+            c = asrow(this.tableTwilite_.coincidences);
+            c = c(ipr.index0:ipr.indexF);
+            c = asrow(c);
+        end
+        function this = decayCorrect(this)
+            %% removes baseline from internal representation then decay-corrects
+            
+            if this.decayCorrected_
+                return
+            end
+            if ~isnice(this.baseline)
+                this.findBaseline(this.datetimeMeasured);
+            end
+            c = this.tableTwilite.coincidences - mean(this.baseline);
+            c = asrow(c) .* 2.^( (this.times - this.timeForDecayCorrection)/this.halflife);
+            this.tableTwilite_.coincidences = ascol(c);
+            this.decayCorrected_ = true;
+        end
+        function this = decayUncorrect(this)
+            if ~this.decayCorrected_
+                return
+            end
+            c = this.tableTwilite.coincidences;
+            c = asrow(c) .* 2.^(-(this.times - this.timeForDecayCorrection)/this.halflife);
+            this.tableTwilite_.coincidences = ascol(c);
+            this.decayCorrected_ = false;
+        end
+        function this = findBaseline(this, doseAdminDatetime1st)
+            %% FINDBASELINE infers baseline from datetimeMeasured to doseAdminDatetime1st,
+            %  but if doseAdminDatetime1st <= datetimeMeasured || countRate('index0', 1, 'indexF', 10) excessively high, 
+            %  infers baseline from last 60 sec of available Twilite countRate.
+            
+            assert(isdatetime(doseAdminDatetime1st) && ~isnat(doseAdminDatetime1st))
+            if this.datetimeMeasured < doseAdminDatetime1st && mean(this.countRate('index0', 1, 'indexF',10)) < this.baselineSup
+                doseAdminIndex1st = round(seconds(doseAdminDatetime1st - this.datetimeMeasured));
+                doseAdminIndex1st = max(doseAdminIndex1st, 1);
+                this.baseline_ = this.countRate('index0', 1, 'indexF',doseAdminIndex1st);
+            else
+                % infer baseline from last 60 sec of twiliteTable                
+                idxF = this.timingData_.indexF;
+                this.baseline_ = this.countRate('index0', idxF-59, 'indexF', idxF);
+            end
+            
+            assert(mean(this.baseline_) < 300, 'mlswisstrace:ValueError', 'TwiliteData.findBaseline')
+        end
+        function this = findBolus(this, doseAdminDatetime)
+            %% FINDBOLUS finds start and termination of bolus; this.index0 := start; this.indexF := termination.
+            %  Manages contexts whereby bolus starts before datastream or bolus terminates after datastream.
+            %
+            %  |bbbb        |  bbbb      |    bbbb    |bbbbbbbb    |.  .. .
+            %  ---------    ---------    ---------    ---------    ---------  
+            
+            assert(isdatetime(doseAdminDatetime) && ~isnat(doseAdminDatetime))
+            doseAdminIndex = round(seconds(doseAdminDatetime - this.datetimeMeasured));
+            doseAdminIndex = max(doseAdminIndex, 1);
+            terminationIndex = min(doseAdminIndex + 600, length(this.times));
+            thresh = 2*mean(this.baseline);
+            
+            % find index just prior to bolus inflow
+            idx0 = 1;
+            while idx0 == 1 && doseAdminIndex > 1                
+                % manage bolus that started prior to start of data
+                doseAdminIndex = doseAdminIndex - 1;
+                [~,idx0] = max(this.countRate('index0', doseAdminIndex, 'indexF',terminationIndex) > thresh);                
+            end
+            this.index0 = doseAdminIndex + idx0 - 1;
+            
+            % find index of bolus peak
+            [~,idxPeak] = max(this.countRate('index0', doseAdminIndex, 'indexF', terminationIndex));
+            
+            % find index just after bolus terminates
+            [~,idxF] = max(this.countRate('index0', doseAdminIndex+idxPeak, 'indexF', this.indexF) < thresh);
+            this.indexF = this.index0 + idxPeak + idxF - 1;
+            if idxF == 1 && this.indexF < this.indices(end)                
+                % manage bolus that persists to end of data
+                this.indexF = this.indices(end);
+            end
+            
+            assert(this.indexF > this.index0, 'mlswisstrace:ValueError', 'TwiliteData.findBolus()')
+        end
+        function this = read(this, varargin)
+            %% updates datetimeMeasured and timingData_.times
+            
+            ip = inputParser;
+            addRequired(ip, 'fqfnCrv', @isfile)
+            parse(ip, varargin{:}); 
+            ipr = ip.Results;
+                      
+            assert(lexist(ipr.fqfnCrv), ...
+                'mlswisstrace.TwiliteData.read could not open %s', ipr.fqfnCrv);
+            assert(~isfolder(ipr.fqfnCrv), ...
+                'mlswisstrace.TwiliteData.read received a path without expected file: %s', ipr.fqfnCrv);
+            tbl = readtable(ipr.fqfnCrv, ...
+                'FileType', 'text', 'ReadVariableNames', false, 'ReadRowNames', false);            
+            dt = datetime(tbl.Var1, tbl.Var2, tbl.Var3, tbl.Var4, tbl.Var5, tbl.Var6, ...
+                'TimeZone', mlpipeline.ResourcesRegistry.instance().preferredTimeZone);
+            coin = tbl.Var7;
+            if length(tbl.Properties.VariableNames) >= 9
+                ch1 = tbl.Var8;
+                ch2 = tbl.Var9;
+            else
+                ch1 = nan(size(coin));
+                ch2 = nan(size(coin));
+            end            
+            this.tableTwilite_ = table(dt, coin, ch1, ch2, ...
+                'VariableNames', {'datetime' 'coincidences' 'channel1' 'channel2'});
+            this.timingData_.datetimeMeasured = dt(1);
+            this.timingData_.times = this.timingData_.timing2num(dt - dt(1));
+        end
+        function this = shiftWorldlines(this, Dt)
+            %% shifts worldline of internal data self-consistently
+            %  @param Dt is numeric.
+            
+            assert(isnumeric(Dt))
+            Dt = asrow(Dt);
+            c = asrow(this.tableTwilite.coincidences);
+            this.tableTwilite_.coincidences = ascol(c .* 2.^(-Dt/this.halflife));
+            this.datetimeMeasured = this.datetimeMeasured + seconds(Dt);
+        end
 		  
  		function this = TwiliteData(varargin)
  			%% TWILITEDATA
- 			%  @param .
-
- 			this = this@AbstractTracerData(varargin{:});
+            %  @param isotope in mlpet.Radionuclides.SUPPORTED_ISOTOPES.  MANDATORY.
+            %  @param tracer.
+            %  @param datetimeMeasured is the measured datetime for times(1).  MANDATORY.
+ 			%  @param datetimeForDecayCorrection.
+            %  @param dt is numeric and must satisfy Nyquist requirements of the client.
+ 			%  @param taus  are frame durations.
+ 			%  @param time0 >= this.times(1).
+ 			%  @param timeF <= this.times(end).
+ 			%  @param times are frame starts.
+            %
+            %  @param pumpRate, default := 5 mL/min.
+            %  @param visibleVolume, default := 0.27 mL, default := 0.14 mL for datetime < 20170412.
+            %  @param activityOverCountRate, an initial scaling, has default := 40.
+            
+            this = this@mlpet.AbstractTracerData(varargin{:});
+            
+            ip = inputParser;
+            ip.KeepUnmatched = true;
+            addParameter(ip, 'pumpRate', 5, @isnumeric)
+            addParameter(ip, 'visibleVolume', NaN, @isnumeric)
+            addParameter(ip, 'activityOverCountRate', 40, @isnumeric)
+            parse(ip, varargin{:})
+            ipr = ip.Results;
+            
+            this.pumpRate_ = ipr.pumpRate;
+            this.visibleVolume_ = ipr.visibleVolume;
+            this.activityOverCountRate_ = ipr.activityOverCountRate;
+            this.decayCorrected_ = false;
  		end
- 	end 
+    end 
+    
+    %% PROTECTED
+    
+    properties (Access = protected)
+        activityOverCountRate_
+        baseline_ % as countRate
+        pumpRate_
+        tableTwilite_
+        visibleVolume_
+    end
 
 	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
  end
-
---
+++ test/+mlswisstrace_unittest/Test_TwiliteData.m
@@ -0,0 +1,185 @@
+classdef Test_TwiliteData < matlab.unittest.TestCase
+	%% TEST_TWILITE 
+
+	%  Usage:  >> results = run(mlswisstrace_unittest.Test_TwiliteData)
+ 	%          >> result  = run(mlswisstrace_unittest.Test_TwiliteData, 'test_dt')
+ 	%  See also:  file:///Applications/Developer/MATLAB_R2014b.app/help/matlab/matlab-unit-test-framework.html
+
+	%  $Revision$
+ 	%  was created 23-Jan-2017 19:39:09
+ 	%  by jjlee,
+ 	%  last modified $LastChangedDate$
+ 	%  and checked into repository /Users/jjlee/MATLAB-Drive/mlswisstrace/test/+mlswisstrace_unittest.
+ 	%% It was developed on Matlab 9.1.0.441655 (R2016b) for MACI64. 	
+
+	properties
+        doseAdminDatetime1st = datetime(2019,5,23,11,29,21, 'TimeZone', mlpipeline.ResourcesRegistry.instance().preferredTimeZone);
+        doseAdminDatetimeOC  = datetime(2019,5,23,12,22,53, 'TimeZone', mlpipeline.ResourcesRegistry.instance().preferredTimeZone);
+        doseAdminDatetimeOO  = datetime(2019,5,23,12,40,17, 'TimeZone', mlpipeline.ResourcesRegistry.instance().preferredTimeZone);
+        doseAdminDatetimeHO  = datetime(2019,5,23,13,0,52,  'TimeZone', mlpipeline.ResourcesRegistry.instance().preferredTimeZone);
+        
+        fqfn_o15_crv = fullfile(getenv('HOME'), 'Documents/private/Twilite/CRV/o15_dt20190523.crv')
+        fqfn_fdg_crv = fullfile(getenv('HOME'), 'Documents/private/Twilite/CRV/fdg_dt20190523.crv')
+        fqfnRadm = fullfile(getenv('HOME'), 'Documents/private/CCIRRadMeasurements 2019may23.xlsx')
+        radm
+ 		registry
+        sesd_fdg
+        sesf_fdg = 'CCIR_00559/ses-E03056/FDG_DT20190523154204.000000-Converted-AC';
+        sesp_fdg = fullfile(getenv('SINGULARITY_HOME'), 'CCIR_00559/ses-E03056/FDG_DT20190523154204.000000-Converted-AC'); 
+        sesd_ho
+        sesf_ho  = 'CCIR_00559/ses-E03056/HO_DT20190523125900.000000-Converted-AC';
+        sesp_ho  = fullfile(getenv('SINGULARITY_HOME'), 'CCIR_00559/ses-E03056/HO_DT20190523125900.000000-Converted-AC');        
+        sesd_oo
+        sesf_oo  = 'CCIR_00559/ses-E03056/OO_DT20190523123738.000000-Converted-AC';
+        sesp_oo  = fullfile(getenv('SINGULARITY_HOME'), 'CCIR_00559/ses-E03056/OO_DT20190523123738.000000-Converted-AC');        
+        sesd_oc
+        sesf_oc  = 'CCIR_00559/ses-E03056/OC_DT20190523122016.000000-Converted-AC';
+        sesp_oc  = fullfile(getenv('SINGULARITY_HOME'), 'CCIR_00559/ses-E03056/OC_DT20190523122016.000000-Converted-AC');
+ 		testObj
+        tracer = 'HO'
+ 	end
+
+	methods (Test)
+		function test_afun(this)
+ 			import mlswisstrace.*;
+ 			this.assumeEqual(1,1);
+ 			this.verifyEqual(1,1);
+ 			this.assertEqual(1,1);
+        end
+        function test_ctor(this)
+            o = this.testObj;
+            this.verifyClass(o, 'mlswisstrace.TwiliteData');
+            disp(o)
+            
+            this.verifyEqual(o.pumpRate, 5)
+            this.verifyEqual(o.tableTwilite{1,'datetime'}, datetime(2019,5,23,13,13,30.804, 'TimeZone', 'America/Chicago'))
+            this.verifyEqual(o.tableTwilite{1,'coincidences'}, 88)
+            this.verifyEqual(o.tableTwilite{1,'channel1'}, 1153)
+            this.verifyEqual(o.tableTwilite{1,'channel2'}, 1065)
+            this.verifyEqual(o.datetimeForDecayCorrection, datetime(2019,5,23,15,42,04, 'TimeZone', 'America/Chicago'))
+            this.verifyEqual(o.halflife, 6.586236000000000e+03)
+            this.verifyEqual(o.isotope, '18F');
+            this.verifyEqual(o.datetimeMeasured, datetime(2019,5,23,13,13,30.804, 'TimeZone', 'America/Chicago'))
+            this.verifyEqual(o.dt, 1)            
+            this.verifyEqual(o.index0, 1);
+            this.verifyEqual(o.indexF, 9526);
+            this.verifyEqual(o.indices, 1:9526);
+            this.verifyEqual(o.taus, ones(1, 9526));
+            this.verifyEqual(o.time0, 0);
+            this.verifyEqual(o.timeF, 9525);
+            this.verifyEqual(o.timeWindow, 9525);
+            this.verifyEqual(o.times, 0:9525);
+
+            this.verifyEqual(size(o.countRate), [1 9526]);
+            this.verifyEqual(size(o.activity), [1 9526]);
+            this.verifyEqual(size(o.activityDensity), [1 9526]);
+        end
+        function test_plot_fdg(this)
+            plot(this.testObj);
+            plot(this.testObj, ...
+                'this.times', ...
+                'this.activityDensity(''decayCorrected'', true)')
+        end
+        function test_plot_ho(this)
+ 			import mlswisstrace.*;			
+ 			o = TwiliteData.createFromSession(this.sesd_ho);
+            o.datetimeForDecayCorrection = this.doseAdminDatetimeHO;
+            disp(o)
+            fprintf('HO datetimeForDecayCorrection: %s\n', o.datetimeForDecayCorrection)
+            fprintf('HO timeForDecayCorrection: %s\n', o.timeForDecayCorrection)
+            plot(o);
+            p = plot(o, ...
+                'this.times(11910:12070)', ...
+                'this.activityDensity(''decayCorrected'', true, ''index0'', 11910, ''indexF'', 12070)');
+        end
+        function test_plot_oo(this)
+ 			import mlswisstrace.*;			
+ 			o = TwiliteData.createFromSession(this.sesd_oo);
+            o.datetimeForDecayCorrection = this.doseAdminDatetimeOO;
+            disp(o)
+            fprintf('OO datetimeForDecayCorrection: %s\n', o.datetimeForDecayCorrection)
+            fprintf('OO timeForDecayCorrection: %s\n', o.timeForDecayCorrection)
+            plot(o);
+            p = plot(o, ...
+                'this.times(10670:10850)', ...
+                'this.activityDensity(''decayCorrected'', true, ''index0'', 10670, ''indexF'', 10850)');
+        end
+        function test_plot_oc(this)
+ 			import mlswisstrace.*;			
+ 			o = TwiliteData.createFromSession(this.sesd_oc);
+            o.datetimeForDecayCorrection = this.doseAdminDatetimeOC;
+            disp(o)
+            fprintf('OC datetimeForDecayCorrection: %s\n', o.datetimeForDecayCorrection)
+            fprintf('OC timeForDecayCorrection: %s\n', o.timeForDecayCorrection)
+            plot(o);
+            p = plot(o, ...
+                'this.times(8550:8710)', ...
+                'this.activityDensity(''decayCorrected'', true, ''index0'', 8550, ''indexF'', 8710)');
+        end
+        function test_findBaseline(this)
+ 			import mlswisstrace.*;			
+ 			o = TwiliteData.createFromSession(this.sesd_oc);
+            o.findBaseline(this.doseAdminDatetime1st);
+            this.verifyEqual(mean(o.baseline), 79.002655005466181, 'RelTol', 1e-12)
+            this.verifyEqual(std( o.baseline), 8.867088941807351,  'RelTol', 1e-12)
+        end
+        function test_findBolus(this)
+ 			import mlswisstrace.*;			
+ 			o = TwiliteData.createFromSession(this.sesd_oc);
+            o.findBaseline(this.doseAdminDatetime1st);
+            o.findBolus(this.doseAdminDatetimeOC);
+            this.verifyEqual(o.index0, 9630)
+            this.verifyEqual(o.indexF, 9914)
+            p = plot(o, ...
+                'this.times(this.index0:this.indexF)', ...
+                'this.activityDensity(''decayCorrected'', true, ''index0'', this.index0, ''indexF'',this.indexF)');
+        end
+        function test_countRate(this)
+        end
+        function test_activityDensity(this)
+        end
+        function test_timeInterpolants(this)
+        end
+        function test_shiftWorldlines(this)
+ 			import mlswisstrace.*;			
+ 			o = TwiliteData.createFromSession(this.sesd_ho);
+            o.datetimeForDecayCorrection = this.doseAdminDatetimeHO;
+            o.index0 = 11910;
+            o.indexF = 12070;
+            disp(o)
+            fprintf('HO datetimeForDecayCorrection: %s\n', o.datetimeForDecayCorrection)
+            fprintf('HO timeForDecayCorrection: %s\n', o.timeForDecayCorrection)
+            plot(o);
+            o.shiftWorldlines(-60);
+            plot(o);
+            title('Test\_TwiliteData.test\_shiftWorldlines()')
+        end
+	end
+
+ 	methods (TestClassSetup)
+		function setupTwilite(this)
+            this.sesd_fdg = mlraichle.SessionData.create(this.sesf_fdg);
+            this.sesd_ho  = mlraichle.SessionData.create(this.sesf_ho);
+            this.sesd_oo  = mlraichle.SessionData.create(this.sesf_oo);
+            this.sesd_oc  = mlraichle.SessionData.create(this.sesf_oc);
+            this.radm     = mlpet.RadMeasurements.createFromSession('sessionData', this.sesd_fdg);
+ 		end
+	end
+
+ 	methods (TestMethodSetup)
+		function setupTwiliteTest(this) 
+ 			import mlswisstrace.*;			
+ 			this.testObj = TwiliteData.createFromSession(this.sesd_fdg);
+            this.testObj.datetimeForDecayCorrection = datetime(this.sesd_fdg);
+ 			this.addTeardown(@this.cleanFiles);
+ 		end
+	end
+
+	methods (Access = private)
+		function cleanFiles(this)
+ 		end
+	end
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
